8fbec0526bc5c22e98889b1ce5307c3b96f22608
APPDE <1657294192@qq.com>
2022-01-26 21:48:06 +0800

    住院

---
+++ src/main/java/com/kkb/controller/beHosController.java
@@ -0,0 +1,44 @@
+package com.kkb.controller;
+
+import com.github.pagehelper.PageInfo;
+import com.kkb.pojo.Behospital;
+import com.kkb.pojo.Hosregister;
+import com.kkb.service.beHosService;
+import com.kkb.vo.QueryHosVO;
+import com.kkb.vo.ResultVO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+/**
+ * @author APPDE
+ */
+@ResponseBody
+@Controller
+@RequestMapping("beHos")
+public class beHosController {
+
+    @Resource
+    private beHosService beHosService;
+
+    @RequestMapping(value = "info",method = RequestMethod.GET)
+    public ResultVO<Behospital> queryByPage(Integer pageNum, QueryHosVO vo){
+        if (pageNum == null || pageNum < 1) {
+            pageNum = 1;
+        }
+        PageInfo<Behospital> behospitalPageInfo = beHosService.queryByPage(pageNum, vo);
+        return new ResultVO<>(behospitalPageInfo);
+    }
+
+    @RequestMapping(value = "{id}",method = RequestMethod.GET)
+    public ResultVO<Behospital> queryBuId(@PathVariable("id") Integer beH_id){
+        Behospital behospital = beHosService.queryById(beH_id);
+        return new ResultVO<>(behospital);
+    }
+
+
+}
---
+++ src/main/java/com/kkb/service/beHosService.java
@@ -0,0 +1,53 @@
+package com.kkb.service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.kkb.mapper.BehospitalMapper;
+import com.kkb.mapper.DoctorMapper;
+import com.kkb.mapper.HosregisterMapper;
+import com.kkb.pojo.*;
+import com.kkb.vo.QueryHosVO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author APPDE
+ */
+@Service
+public class beHosService {
+
+    @Resource
+    private DoctorMapper doctorMapper;
+
+    @Resource
+    private BehospitalMapper behospitalMapper;
+
+    @Resource
+    private HosregisterMapper hosregisterMapper;
+
+    @Transactional(propagation = Propagation.REQUIRED,readOnly = true)
+    public PageInfo<Behospital> queryByPage(Integer pageNum, QueryHosVO vo) {
+        BehospitalExample hosregisterExample = new BehospitalExample();
+        BehospitalExample.Criteria criteria = hosregisterExample.createCriteria();
+        PageHelper.startPage(pageNum, 5);
+        List<Behospital> behospitals = behospitalMapper.selectByExample(hosregisterExample);
+        behospitals.forEach(behospital -> {
+            behospital.setHosregister(hosregisterMapper.selectByPrimaryKey(behospital.getBeH_id()));
+            behospital.getHosregister().setDoctor(doctorMapper.selectByPrimaryKey(behospital.getHosregister().getD_id()));
+        });
+        return new PageInfo<>(behospitals);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED,readOnly = true)
+    public Behospital queryById(Integer beH_id){
+        Behospital behospital = behospitalMapper.selectByPrimaryKey(beH_id);
+        behospital.setHosregister(hosregisterMapper.selectByPrimaryKey(beH_id));
+        behospital.getHosregister().setDoctor(doctorMapper.selectByPrimaryKey(behospital.getHosregister().getD_id()));
+        return behospital;
+    }
+
+}
